4b29a805e4e7057739815bb3cedc672e7b037c4a
marmeden <itsme@eneasmarin.es>
2018-10-08 17:58:32 +0200

    Firebase Working

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
-  </div>
+  <v-app>    
+    <toolbar
+      :login="login"
+      :logout="logout"
+      :profile="profile"
+      :loggedIn="loggedIn"
+    />
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
+import Toolbar from '@/components/Toolbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { Toolbar },
+  computed: {
+    ...mapGetters('user', {
+      profile: 'profile',
+      loggedIn: 'loggedIn'
+    })
+  },
+  methods: {
+    ...mapActions('user', {
+      login: 'login',
+      logout: 'logout'
+    })
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'Vuetify'
+
 import App from './App'
 import router from './router'
+import store from './store'
+import './auth'
 
-Vue.config.productionTip = false
+import('vuetify/dist/vuetify.min.css')
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
