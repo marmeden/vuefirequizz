b44dcd94a03482c04957f0a52b6d23cf8ff5a802
marmeden <itsme@eneasmarin.es>
2018-10-09 13:35:43 +0200

    Saving to firebase

--- src/store/quiz/index.js
+++ src/store/quiz/index.js
@@ -1,17 +1,141 @@
+import db from '@/db'
+import firebase from '@/firebase'
+
+import {
+  UPDATE_INFORMATION,
+  ADD_ANSWER,
+  REMOVE_QUESTION,
+  REMOVE_ANSWER,
+  UPDATE_ANSWER,
+  RESET_QUIZ,
+  UPDATE_QUESTION,
+  ADD_QUESTION
+} from './mutations'
+
 const state = {
-  quiz: 'a funny quiz'
+  newQuiz: {
+    title: 'Quiz 2018',
+    description: 100,
+    questions: [
+      {
+        question: 'First question',
+        points: 'First points',
+        answers: [
+          {
+            answer: 'First answer'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 const getters = {
-
+  newQuiz: ({newQuiz}) => newQuiz
 }
 
 const mutations = {
+  [RESET_QUIZ] (state) {
+    this.newQuiz = {
+      title: '',
+      description: '',
+      questions: []
+    }
+  },
+  [UPDATE_INFORMATION] (state, info) {
+    state.newQuiz.title = info.title
+    state.newQuiz.description = info.description
+  },
 
+  [ADD_ANSWER] (state, questionIndex) {
+    const answers = state.newQuiz.questions[questionIndex].answers
+    if (answers.length < 5) {
+      answers.push({
+        answer: 'Anotha one!',
+        isRight: false
+      })
+    }
+  },
+
+  [REMOVE_QUESTION] (state, questionIndex) {
+    if (state.newQuiz.questions.length > 1) {
+      state.newQuiz
+        .questions
+        .splice(questionIndex, 1)
+    }
+  },
+
+  [REMOVE_ANSWER] (state, payload) {
+    const questionIndex = payload.questionIndex
+    const answerIndex = payload.answerIndex
+
+    const question = state.newQuiz.questions[questionIndex]
+
+    if (question.answers.length > 1) {
+      question.answers.splice(answerIndex, 1)
+    }
+  },
+
+  [UPDATE_ANSWER] (state, payload) {
+    const questionIndex = payload.questionIndex
+    const answerIndex = payload.answerIndex
+    const answerText = payload.answer
+    const isRight = payload.isRight
+
+    const answer = state.newQuiz
+      .questions[questionIndex]
+      .answers[answerIndex]
+
+    answer.isRight = isRight
+    answer.answer = answerText
+  },
+
+  [UPDATE_QUESTION] (state, payload) {
+    const question = state.newQuiz.questions[payload.questionIndex]
+    question.question = payload.question
+    question.points = payload.points
+  },
+
+  [ADD_QUESTION] (state) {
+    state.newQuiz
+      .questions
+      .push({
+        question: 'Question',
+        points: 0,
+        answers: []
+      })
+  }
 }
 
 const actions = {
+  async create ({state}) {
+    const user = firebase.auth().currentUser
+    if (user) {
+      // check if there is a question without a right answer
+      state.newQuiz.questions.map(question => {
+        let hasRightAnswer = false
 
+        question.answers.map(answer => {
+          if (answer.isRight) hasRightAnswer = true
+        })
+
+        if (!hasRightAnswer) {
+          alert(`Question: '${question.question}' doesn't have a right answer!`)
+          throw new Error()
+        }
+      })
+
+      // save to database
+      await db.collection('quizes').add({
+        ...state.newQuiz,
+        userId: user.uid
+      })
+
+      alert('Quiz created')
+    } else {
+      alert('Unauthorized')
+    }
+  }
 }
 
 export default {
